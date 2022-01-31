b8e43e8c37a2d32a1b889e1476e9b6ed0ec160a4
Darshit Sorathiya <darshitgsorathiya7@gmail.com>
2022-01-31 18:44:10 +0530

    till refactoring News component for webpage title change

--- src/Components/News.js
+++ src/Components/News.js
@@ -15,14 +15,19 @@ export class News extends Component {
     pageSize:PropTypes.number,
    category:PropTypes.string,
   }
-  constructor() {
-    super();
+
+  capitalizeFirstLetter=(string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  constructor(props) {
+    super(props);
     //console.log("Hello I am a constructor from News component");
     this.state = {
       articles: [],
       loading: false,
       page: 1,
-    };
+    }
+    document.title=`${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
 
   async updateNews(){
@@ -60,7 +65,7 @@ export class News extends Component {
     //console.log("render");
     return (
       <div className="container">
-        <h1 className="text-center my-3">News Monkey - Top Headlines</h1>
+        <h1 className="text-center my-3">News Monkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines  </h1>
         {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.loading&&this.state.articles.map((element) => {
